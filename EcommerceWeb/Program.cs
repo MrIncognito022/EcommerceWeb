64eeff9d5488173af7d4751df05d8937e18e6e2d
Qureshi <mr.incognito022@gmail.com>
2023-01-01 02:36:50 -0800

    Added Cateories Model

--- EcommerceWeb/Program.cs
+++ EcommerceWeb/Program.cs
@@ -12,6 +12,8 @@ namespace EcommerceWeb
             bool isManageCategory = false;
             bool isProductLoopTerminate = false;
             Products[] products = new Products[100];
+            Category[] categories = new Category[10];
+            int categoryIndex = 0;
             int ProductIndex = 0;
 
 
@@ -29,20 +31,31 @@ namespace EcommerceWeb
                 {
                     do
                     {
-                        Console.WriteLine("Choose the Option \n \t 1) Add Category \n \t 2) Edit Category 3) Update Category");
+                        Console.WriteLine("Choose the Option \n \t 1) Add Category \n \t 2) Display All Categories \n \t 3) Update Category");
                         var input1 = Console.ReadLine();
                         switch (input1)
                         {
                             case "1":
                                 {
+                                    if (categoryIndex < categories.Length)
+                                    {
+                                        CategoryRepository.AddCategory(categories, categoryIndex);
+                                        categoryIndex++;
+                                    }
+                                    else
+                                    {
+                                        Console.WriteLine("Limit is exceed");
+                                    }
                                     break;
                                 }
                             case "2":
                                 {
+                                    CategoryRepository.DisplayAllCategory(categories, categoryIndex);
                                     break;
                                 }
                             case "3":
                                 {
+                                    CategoryRepository.UpdateCategory(categories, categoryIndex);
                                     break;
                                 }
                             default:
--- Infrastructure/CategoryRepository.cs
+++ Infrastructure/CategoryRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DBModels;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,6 +7,53 @@ namespace Infrastructure
 {
     public class CategoryRepository
     {
+        public static Category[] AddCategory(Category[] categories, int index)
+        {
+            Console.Clear();
+            Category category = new Category();
+            Console.WriteLine("-----------Add Category----------\n");
+            Console.WriteLine("Enter the Category Name");
+            category.CategoryName = Console.ReadLine();
+            categories[index] = category;
+            Console.WriteLine("Product Added Successfully");
+            return categories;
+
+            //--------------Display Products------------
+
+
+        }
+
+        public static void DisplayAllCategory(Category[] categories, int Index)
+        {
+            Console.Clear();
+            for (int i = 0; i < Index; i++)
+            {
+                Console.WriteLine($"Product at Index {i + 1} is {categories[i].CategoryName}");
+            }
+        }
+
+
+        public static Category[] UpdateCategory(Category[] categories, int index)
+        {
+            Console.Clear();
+            Console.WriteLine("Enter index of category that you want to Update;");
+            int EditIndex = Convert.ToInt32(Console.ReadLine());
+            if (EditIndex <= index)
+            {
+
+                //if (books[EditIndex -1].Title != null)
+                //{
+
+                //}
+                Console.WriteLine("Enter category Name to update");
+                categories[EditIndex - 1].CategoryName = Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("Invalid entry!");
+            }
+            return categories;
+        }
 
 
     }
